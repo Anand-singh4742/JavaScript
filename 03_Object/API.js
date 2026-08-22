d072237a8b6eaacc0d072e4ebae0eebf0d2ef32f
Anand Singh <his-email@example.com>
2026-08-22 12:23:03 +0530

    API and JSON - very basic's

---
+++ 03_Object/API.js
@@ -0,0 +1,96 @@
+//API -- Jab bhi haame aapna kaam kisi aur pe daal dena hai 
+// JSON object he hauta hai jo key value pair ki values me data rakhta hai
+
+// {
+    // example of empty JSON -- haa yahi hai JSON
+// }
+
+// {
+//     "course": "JS in Hindi",            //JSON me key and value dono string me hauti hai 
+//     "name": "Anand",
+//     "price": "free"
+// } 
+
+//api.github.com/users/hiteshchoudhary -- example hai API ka dekh lena 
+
+// What i understood !!
+// haam js me API call karte hai -- kisi bhi url se and wo haame waha se data laake deta hai and ekh JSON file me deta hai and 
+// jisse further haam object easily bana sakte haam fir and usse easily data fetch kar sakte...!!
+
+//Zaruri nahi API sirf object ke form me mile kafi baar Array ke form me bhi milti hai jiskey ander objects haute
+// [
+//     {},
+//     {}
+// ]
+
+//Example of an API -- https://randomuser.me/ 
+
+// Bhout he complex si API's hauti jinpey samajne ke liye time dena padta -- inhi ko samajne ke liye kafi saare tools available hai -- json formatter
+//https://jsonformatter.org/
+
+// {
+//   "results": [
+//     {
+//       "gender": "female",
+//       "name": {
+//         "title": "Miss",
+//         "first": "Jennie",
+//         "last": "Nichols"
+//       },
+//       "location": {
+//         "street": {
+//           "number": 8929,
+//           "name": "Valwood Pkwy",
+//         },
+//         "city": "Billings",
+//         "state": "Michigan",
+//         "country": "United States",
+//         "postcode": "63104",
+//         "coordinates": {
+//           "latitude": "-69.8246",
+//           "longitude": "134.8719"
+//         },
+//         "timezone": {
+//           "offset": "+9:30",
+//           "description": "Adelaide, Darwin"
+//         }
+//       },
+//       "email": "jennie.nichols@example.com",
+//       "login": {
+//         "uuid": "7a0eed16-9430-4d68-901f-c0d4c1c3bf00",
+//         "username": "yellowpeacock117",
+//         "password": "addison",
+//         "salt": "sld1yGtd",
+//         "md5": "ab54ac4c0be9480ae8fa5e9e2a5196a3",
+//         "sha1": "edcf2ce613cbdea349133c52dc2f3b83168dc51b",
+//         "sha256": "48df5229235ada28389b91e60a935e4f9b73eb4bdb855ef9258a1751f10bdc5d"
+//       },
+//       "dob": {
+//         "date": "1992-03-08T15:13:16.688Z",
+//         "age": 30
+//       },
+//       "registered": {
+//         "date": "2007-07-09T05:51:59.390Z",
+//         "age": 14
+//       },
+//       "phone": "(272) 790-0888",
+//       "cell": "(489) 330-2385",
+//       "id": {
+//         "name": "SSN",
+//         "value": "405-88-3636"
+//       },
+//       "picture": {
+//         "large": "https://randomuser.me/api/portraits/men/75.jpg",
+//         "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
+//         "thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+//       },
+//       "nat": "US"
+//     }
+//   ],
+//   "info": {
+//     "seed": "56d27f4a53bd5441",
+//     "results": 1,
+//     "page": 1,
+//     "version": "1.4"
+//   }
+// }
